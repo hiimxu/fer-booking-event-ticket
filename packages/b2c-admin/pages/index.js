17981dde16266383c331a00a369c1c281150fbf6
xu <hieudd0899@gmail.com>
2024-02-23 22:03:06 +0700

    Create codebase

--- packages/b2c-admin/pages/index.js
+++ packages/b2c-admin/pages/index.js
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Admin
-    </main>
-  );
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        >
+            Admin
+        </main>
+    );
 }
