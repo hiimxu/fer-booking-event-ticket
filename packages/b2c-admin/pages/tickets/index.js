b073b50e33e21fc99379d17c6045a30014fbb3eb
LinhBX <Linhbxhe160309@fpt.edu.vn>
2024-03-06 13:25:39 +0700

    create: tikets

--- packages/b2c-admin/pages/tickets/index.js
+++ packages/b2c-admin/pages/tickets/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import { Table, Space, Spin, Tag } from 'antd';
+import Container from '~/components/container';
+import { Header } from '~/components/header';
+import { useQuery } from 'common/hooks/useQuery';
 
 const Tickets = () => {
-    return <div>Tickets</div>;
+    const { isLoading, data, reload } = useQuery('eventType');
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Show on select',
+            dataIndex: 'isShow',
+            key: 'isShow',
+            render: (_, { isShow }) => (
+                <>
+                    <Tag color={isShow === '1' ? 'green' : 'red'}>
+                        {isShow === '1' ? 'SHOW' : 'HIDE'}
+                    </Tag>
+                </>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    {/* <EventTypeForm
+                        typeId={record?.id}
+                        title="Edit event type"
+                        successCallback={reload}
+                    /> */}
+                </Space>
+            ),
+        },
+    ];
+    return (
+        <Spin spinning={isLoading}>
+            <Container>
+                <div className="flex justify-between">
+                    <Header title="Events Type Management" />
+                    {/* <EventTypeForm
+                        successCallback={reload}
+                        label="Create"
+                        title="Create event type"
+                    /> */}
+                </div>
+                <Table
+                    pagination={{ pageSize: 5 }}
+                    dataSource={data}
+                    columns={columns}
+                />
+            </Container>
+        </Spin>
+    );
 };
 
 export default Tickets;
